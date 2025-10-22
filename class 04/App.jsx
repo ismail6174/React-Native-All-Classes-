d730f00c659b2171a4456bef749422f219a031ef
Ismail Shah <ismailpx.6174@gmail.com>
2025-10-22 23:58:29 +0500

    Add files via upload

---
+++ class 04/App.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'  
+import App_Navigation from './config/App_Navigation'
+
+const App = () => {
+   return <App_Navigation />
+}
+
+export default App    
+
+// cd  android 
+//  gradlew clean    
+// cd  ..
+
+//  cd android && gradlew clean && cd ..
---
+++ class 04/components/Card.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { Text, View } from "react-native"
+
+const Card = () => {
+    const [list, setList] = useState([
+        '1',   
+        '2',
+        '3',    
+        '4',
+        '5',
+    ])
+
+    return <>
+    <View>
+        <Text>List</Text>
+    </View>
+
+    <View>
+        {
+            list.map((item)=>{
+                return <Text>
+                    {item}
+                </Text>
+            })
+        }   
+    </View>
+    </>
+
+
+}
+
+
+export {Card}
